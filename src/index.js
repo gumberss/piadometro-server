d51f172fda852620a7a09f9250b5a22566c00d36
Gustavo Marin Suppi <gustavomarinsuppi@gmail.com>
2018-11-20 23:58:49 -0200

    adicionando configuração

--- src/index.js
+++ src/index.js
@@ -18,6 +18,6 @@ app.use(cors())
 app.use(express.json())
 app.use(require('./routes'))
 
-server.listen(3000, () => {
+server.listen(process.env.PORT || 3000, () => {
     console.log('Server started on port 3000')
 })
